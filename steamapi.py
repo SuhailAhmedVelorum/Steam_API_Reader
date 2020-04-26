9f6879c1ff3edde3c8dbb5254a2c9c161b31194c
SuhailAhmedVelorum <61581649+SuhailAhmedVelorum@users.noreply.github.com>
2020-04-26 15:02:34 +0530

    Update steamapi.py

--- steamapi.py
+++ steamapi.py
@@ -20,75 +20,32 @@ def weapon():
         "BS": "Battle-Scarred"                     
     }
     EXTERIOR = d[EXTERIOR]
-    responsew = requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24")   
-    print(responsew.json())
-    print(responsew.json()['lowest_price'])
-    print(responsew.json()['volume'])
+    root = Tk()
+    root.title(WEAPON +" "+ SKIN + " | " + EXTERIOR)
+    lbl = Label(root, font = ('source code pro', 20, 'bold'), background = 'black', foreground = 'pink')
+    lbl.config(text = 'Prices start at ' + requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24").json()['lowest_price']+ "\n" + 'Currently '+ requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24").json()['volume'] + ' units are available in the market')
+    lbl.pack(anchor = 'center')
+    root.mainloop()
 
 def agent():
     AGENT = input("Agent: ")
-    GROUP = input("GROUP: ")
-    responsea = requests.get("https://steamcommunity.com/market/priceoverview/?currency=24&appid=730&market_hash_name=" + AGENT + "%20%7C%20" + GROUP)
-    print(responsea.json())
-    print(responsea.json()['lowest_price'])
-    print(responsea.json()['volume'])
+    GROUP = input("Group: ")
+    root = Tk()
+    root.title(AGENT + " | " + EXTERIOR)
+    lbl = Label(root, font = ('source code pro', 20, 'bold'), background = 'black', foreground = 'pink')
+    lbl.config(text = 'Prices start at '+ requests.get("https://steamcommunity.com/market/priceoverview/?currency=24&appid=730&market_hash_name=" + AGENT + "%20%7C%20" + GROUP).json()['lowest_price']+ "\n" + 'Currently '+ requests.get("https://steamcommunity.com/market/priceoverview/?currency=24&appid=730&market_hash_name=" + AGENT + "%20%7C%20" + GROUP).json()['volume'] + ' units are available in the market')
+    lbl.pack(anchor = 'center')
+    root.mainloop()
 
 def case():
     CASE = input("Case: ")
-    responsec = requests.get("https://steamcommunity.com/market/priceoverview/?currency=24&appid=730&market_hash_name=" + CASE)
-    print(responsec.json())
-    print(responsec.json()['lowest_price'])
-    print(responsec.json()['volume'])
+    root = Tk()
+    root.title(CASE + " Case")
+    lbl = Label(root, font = ('source code pro', 20, 'bold'), background = 'black', foreground = 'pink')
+    lbl.config(text = 'Prices start at '+ requests.get("https://steamcommunity.com/market/priceoverview/?currency=24&appid=730&market_hash_name=" + CASE).json()['lowest_price'] + "\n" + 'Currently '+ requests.get("https://steamcommunity.com/market/priceoverview/?currency=24&appid=730&market_hash_name=" + CASE).json()['volume'] + ' units are available in the market')    
+    lbl.pack(anchor = 'center')    
+    root.mainloop()
 
-def weaponexc():
-    WEAPON = input("Weapon name: ")
-    SKIN = input("Skin: ")
-    EXTERIOR = input("Exterior type (FN/MW/FT/WW/BS): ")
-    WEAPON = re.sub(r"\s+", '%20', WEAPON)
-    d = {
-            "FN": "Factory New",
-            "MW": "Minimal Wear",
-            "FT": "Field-Tested",
-            "WW": "Well-Worn",
-            "BS": "Battle-Scarred"
-            }
-    EXTERIOR = d[EXTERIOR]
-    responsew = requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24")
-    print(responsew.json()['lowest_price'])
-    
-
-def agentexc():
-    print()
-
-def caseexc():
-    print()
-
-def inv():
-    print()
-
-def exc():
-    global tries
-    una = input("\nUsername: ")
-    pas = getpass.getpass()  
-    if una == "root" and pas == "NoPe":
-        TYPEn = input("\nChoose type from (Weapon/Agent/Case/Inv): ")
-        if TYPEn == "weapon":
-            waeponexc()    
-        if TYPEn == "agent":
-            agentexc()
-        if TYPEn == "case":
-            caseexc()
-        if TYPEn == "inv":
-            inv()
-    else:
-        if tries > 1:
-            tries-=1   
-            print('Wrong username or password')
-            exc()
-        else:  
-            print("\nWe couldn't verify if you're an account holder. Kindly contact the owner for help. :) ")
-            time.sleep(2)
-            sys.exit()
 def init_input():   
     TYPE = input("Choose type from (Weapon/Agent/Case): ")
     if TYPE == "weapon":
@@ -97,8 +54,13 @@ def init_input():
         agent()
     elif TYPE == "case":
         case()
-    elif TYPE == "exc":
-        exc()
     else:
-        ctypes.windll.user32.MessageBoxW(0, "Please enter a valid option", "INCORRECT RESPONSE !", 0)
+        root = Tk()
+        root.title('INCORRECT RESPONSE !')
+        lbl = Label(root, font = ('source code pro', 20, 'bold'), background = 'black', foreground = 'pink') 
+        lbl.config(text = 'Please enter a valid response.')
+        lbl.pack(anchor = 'center')
+        root.mainloop()
         init_input()
+
+init_input()

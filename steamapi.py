a92b0f46cac85d4382f3a60a3d4aaa732157af13
AshiqAbdulkhader <57718729+AshiqAbdulkhader@users.noreply.github.com>
2020-10-11 19:09:04 +0530

    Update steamapi.py

--- steamapi.py
+++ steamapi.py
@@ -14,19 +14,8 @@ def weapon():
     STAT = input('StatTrak (Y/N): ').lower()
     WEAPONS = WEAPON
     WEAPON = re.sub(r"\s+", '%20', WEAPON)
-    if STAT == 'y':
-        WEAPON = "StatTrak%E2%84%A2 "+ WEAPON
-    d = {
-        "fn": "Factory New",
-        "mw": "Minimal Wear",
-        "ft": "Field-Tested",
-        "ww": "Well-Worn", 
-        "bs": "Battle-Scarred"                     
-    }
-    EXTERIOR = d[EXTERIOR]
-    root = Tk()
-    if STAT == y:
-        root.title("StatTrak " +WEAPONS +" "+ SKIN + " | " + EXTERIOR)
+    if STAT == "y" or "Y" or "Yes" or "yes":
+        root.title("StatTrak " + WEAPONS +" "+ SKIN + " | " + EXTERIOR)
     lbl = Label(root, font = ('source code pro', 20, 'bold'), background = 'black', foreground = 'pink')
     lbl.config(text = 'Prices start at ' + requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24").json()['lowest_price']+ "\n" + 'Currently '+ requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24").json()['volume'] + ' units are available in the market')
     lbl.pack(anchor = 'center')

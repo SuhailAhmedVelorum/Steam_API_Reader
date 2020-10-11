8cc22879086c11f75605f55c7c4241c0371fd0c4
SuhailAhmedVelorum <61581649+SuhailAhmedVelorum@users.noreply.github.com>
2020-10-11 19:18:36 +0530

    Managed stattrak variable better

--- steamapi.py
+++ steamapi.py
@@ -5,7 +5,6 @@ import sys
 import time
 import ctypes
 from tkinter import *
-# HELLO i am part of master
 
 def weapon():
     WEAPON = input("Weapon name: ")
@@ -14,7 +13,7 @@ def weapon():
     STAT = input('StatTrak (Y/N): ').lower()
     WEAPONS = WEAPON
     WEAPON = re.sub(r"\s+", '%20', WEAPON)
-    if STAT == "y" or "Y" or "Yes" or "yes":
+    if STAT == "y" :
         root.title("StatTrak " + WEAPONS +" "+ SKIN + " | " + EXTERIOR)
     lbl = Label(root, font = ('source code pro', 20, 'bold'), background = 'black', foreground = 'pink')
     lbl.config(text = 'Prices start at ' + requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24").json()['lowest_price']+ "\n" + 'Currently '+ requests.get("https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=" + WEAPON + "%20%7C%20" + SKIN + "%20" + "%28" + EXTERIOR + "%29" + "&currency=24").json()['volume'] + ' units are available in the market')
